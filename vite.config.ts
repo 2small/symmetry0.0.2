fb2c1dde2d9a35ebd0ff83ed7a213306d56b2c3d
2small <greenpurpleveins@gmail.com>
2026-06-26 13:24:02 +0300

    Update vite.config.ts src

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src', // 👈 Redirects Vite's entry point to the src folder
+  build: {
+    outDir: '../dist', // 👈 Forces the compiled build to output to the project root's dist folder
+    emptyOutDir: true
+  }
 })
