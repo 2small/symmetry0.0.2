259532c049322483e3b7fc668e57922a39b5b461
2small <greenpurpleveins@gmail.com>
2026-06-26 13:30:21 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src', // 👈 Redirects Vite's entry point to the src folder
+  root: 'src/symmetry', // 👈 Redirects Vite's entry point to the src folder
   build: {
     outDir: '../dist', // 👈 Forces the compiled build to output to the project root's dist folder
     emptyOutDir: true
